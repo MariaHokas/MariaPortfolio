e3bc2ee8cc8415e3ac96b4ffb9506e80e4a6ef9e
Ahokas Maria Katariina <maria.ahokas@stud.pointcollege.fi>
2018-09-23 18:43:19 +0300

    Second day for my portfolio

---
+++ WebMaPortfolio/Controllers/BolgsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMaPortfolio.Controllers
+{
+    public class BolgsController : Controller
+    {
+        // GET: Bolgs
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
